872f4f70fe87e6198be93a5a47854ca9cd802d34
q84mahdi <ghasemi84mahdi@gmail.com>
2026-02-06 10:57:27 +0330

    Convert middleware and auth utility functions to TypeScript

--- src/utils/setCookiesOnReq.ts
+++ src/utils/setCookiesOnReq.ts
@@ -1,13 +1,13 @@
-export default function setCookiesOnReq(cookies) {
+import { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
+
+export default function setCookiesOnReq(cookies: RequestCookies): RequestInit {
   const accessToken = cookies.get("accessToken");
   const refreshToken = cookies.get("refreshToken");
 
-  const options = {
+  return {
     credentials: "include",
     headers: {
       Cookie: `${accessToken?.name}=${accessToken?.value}; ${refreshToken?.name}=${refreshToken?.value}`,
     },
   };
-
-  return options;
 }
